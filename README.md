efdc237bd893ff22858956d07041722c1df84c1f
Bruno <brunocruz012013@gmail.com>
2023-03-22 14:47:24 -0300

    Adicionando arquivos do projeto

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# weather-using-api
-☂️  Aplicação com OpenWeather API
+## 📌 OpenWeatherMap
+
+- É um serviço online, de propriedade da OpenWeather, que fonece dados meteorológicos globais via API, incluindo dados meteorológicos atuais, previsões e dados meteorológicos, históricos para qualquer localização geográfica.
+
+## 📚 Materiais
+
+- CSS (Cascading Style Sheets)
+- JS (JavaScript)
+- HTML (HyperText Markup Language)
+- Weather API (https://openweathermap.org/api)
+- Google Fonte (https://fonts.google.com/)
+- Font Awesome (https://fontawesome.com/)
+
+## 📁 Projeto
+### 📍 Fazendo o clone do repositório:
+
+    git clone https://github.com/brunnuscz/weather-using-api.git
+
+### 📍 Entrando na pasta do projeto. Para acessar basta abrir o arquivo `index.html` no navegador:
+    
+    cd weather-using-api
+
+## 🚀 Final
+- Para que a aplicação funcione corretamente na sua máquina, é preciso que crie uma conta na OpenWeatherMap, vai ser gerado uma chave. Vá em `nome do usuário`, clique em `My API keys` copie e cole a chave no arquivo `script.js`.
---
+++ js/script.js
@@ -0,0 +1,65 @@
+// váriaveis e seleção de elementos
+// adicione entre as aspas a sua chave
+const apiKey = "";
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+const cityElement = document.querySelector("#city");
+const tempMaxElement = document.querySelector("#temperature-max span");
+const tempMinElement = document.querySelector("#temperature-min span");
+const descElement = document.querySelector("#description");
+const countryElement = document.querySelector("#country");
+const weatherIconElement = document.querySelector("#weather-icon");
+const humidityElement = document.querySelector("#humidity span");
+const windElement = document.querySelector("#wind span");
+
+const weatherContainer = document.querySelector("#weather-data");
+const msgError = document.querySelector("#msg-error");
+
+// funções
+const getWeatherData = async(city) => {
+    const apiWeatherApiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+    const res = await fetch(apiWeatherApiURL);
+    const data = await res.json();
+    console.log(data);
+    if(data.name != undefined){
+        return data;
+    }else{
+        weatherContainer.classList.add("hide-1");
+        msgError.classList.remove("hide-2");
+    }
+}
+const showWeatherData = async (city) => {
+    const data = await getWeatherData(city);
+    cityElement.innerText = data.name;
+    tempMaxElement.innerHTML = parseInt(data.main.temp_max);
+    tempMinElement.innerHTML = parseInt(data.main.temp_min);
+    descElement.innerText = data.weather[0].description;
+    countryElement.innerText = data.sys.country;
+    weatherIconElement.setAttribute(
+        "src",
+        `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+    );
+    humidityElement.innerText = `${data.main.humidity}%`;
+    windElement.innerText = `${data.wind.speed}km/h`;
+    
+    weatherContainer.classList.remove("hide-1");
+    msgError.classList.add("hide-2");
+}
+
+// eventos
+searchBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const city = cityInput.value;
+    showWeatherData(city);
+});
+
+cityInput.addEventListener("keyup", (e) => {
+    if(e.code === "Enter"){
+        const city = e.target.value;
+        showWeatherData(city);
+    }
+});
+
+
